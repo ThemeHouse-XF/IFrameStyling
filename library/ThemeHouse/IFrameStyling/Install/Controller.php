92479793a20b533590df3e95d2ee5c64e2454535
Jake B <jake.booher@gmail.com>
2015-11-23 10:53:16 -0600

    Initial Commit

---
+++ library/ThemeHouse/IFrameStyling/Install/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ThemeHouse_IFrameStyling_Install_Controller extends ThemeHouse_Install
+{
+
+    protected $_resourceManagerUrl = 'https://xenforo.com/community/resources/transmogrifier-by-th.4163/';
+
+    protected $_minVersionId = 1020000;
+
+    protected $_minVersionString = '1.2.0';
+}
